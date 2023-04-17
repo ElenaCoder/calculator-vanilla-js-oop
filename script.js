b9f6fa1b33e81107c29a14e13114ed9e18627997
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-04-17 21:50:10 +0300

    feat: create variables of DOM elements for manipulation

---
+++ script.js
@@ -0,0 +1,7 @@
+const operatorBtns = document.querySelectorAll('[data-operator]');
+const numberBtns = document.querySelectorAll('[data-number]');
+const allClearBtn = document.querySelector('[data-all-clear]');
+const deleteBtn = document.querySelector('[data-delete]');
+const equalBtn = document.querySelectorAll('[data-equal]');
+const previousOperandTextElem = document.querySelectorAll('[data-previous-operand]');
+const currentOperandTextElem = document.querySelectorAll('[data-current-operand]');
