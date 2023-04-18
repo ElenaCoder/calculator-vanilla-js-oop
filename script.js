6d1e38cf055efbb231d989edc51f5b7793c61f69
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-04-18 15:47:41 +0300

    feat: add eventListener with handler to equal btn

--- script.js
+++ script.js
@@ -3,24 +3,32 @@ const numberBtns = document.querySelectorAll('[data-number]');
 const allClearBtn = document.querySelector('[data-all-clear]');
 const deleteBtn = document.querySelector('[data-delete]');
 const equalBtn = document.querySelector('[data-equal]');
-const previousOperandTextElem = document.querySelector('[data-previous-operand]');
+const previousOperandTextElem = document.querySelector(
+    '[data-previous-operand]',
+);
 const currentOperandTextElem = document.querySelector('[data-current-operand]');
 
-
-const calculator = new Calculator(previousOperandTextElem, currentOperandTextElem);
+const calculator = new Calculator(
+    previousOperandTextElem,
+    currentOperandTextElem,
+);
 // console.log(calculator);
 
-
-operatorBtns.forEach(btn => {
+operatorBtns.forEach((btn) => {
     btn.addEventListener('click', () => {
         calculator.chooseOperation(btn.innerText);
         calculator.updateDisplay();
-    })
-})
+    });
+});
 
-numberBtns.forEach(btn => {
+numberBtns.forEach((btn) => {
     btn.addEventListener('click', () => {
         calculator.appendNumber(btn.innerText);
         calculator.updateDisplay();
-    })
-})
+    });
+});
+
+equalBtn.addEventListener('click', (btn) => {
+    calculator.compute();
+    calculator.updateDisplay();
+});
