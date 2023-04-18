484b7f3b5090496309c700238939e179b278cf10
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-04-18 13:31:44 +0300

    feat: add eventListeners to all number buttons;

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const operatorBtns = document.querySelectorAll('[data-operator]');
 const numberBtns = document.querySelectorAll('[data-number]');
 const allClearBtn = document.querySelector('[data-all-clear]');
 const deleteBtn = document.querySelector('[data-delete]');
-const equalBtn = document.querySelectorAll('[data-equal]');
-const previousOperandTextElem = document.querySelectorAll('[data-previous-operand]');
-const currentOperandTextElem = document.querySelectorAll('[data-current-operand]');
+const equalBtn = document.querySelector('[data-equal]');
+const previousOperandTextElem = document.querySelector('[data-previous-operand]');
+const currentOperandTextElem = document.querySelector('[data-current-operand]');
+
+
+const calculator = new Calculator(previousOperandTextElem, currentOperandTextElem);
+// console.log(calculator);
+
+
+numberBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        calculator.appendNumber(btn.innerText);
+        calculator.updateDisplay();
+    })
+})
